bbbfb19cc3016c5aabeafab3eeda00d96a16ea67
Kaio Menezes <73968667+kaiomenezes57@users.noreply.github.com>
2025-11-02 18:16:34 -0300

    add scene lifetime scope

---
+++ Assets/_Game/_Scripts/Services/DI/SceneLifetimeScope.cs
@@ -0,0 +1,12 @@
+using VContainer;
+using VContainer.Unity;
+
+namespace Game.Services.DI
+{
+    public sealed class SceneLifetimeScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+        }
+    }
+}
